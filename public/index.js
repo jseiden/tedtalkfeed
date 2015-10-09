c49687c5b6aa9d8b57b5155b085dd4353fa3f21c
jseiden <jseiden@gmail.com>
2015-10-09 16:54:19 -0700

    add CSS

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+
+
 google.load("feeds", "1");
 
 function initialize() {
@@ -8,7 +10,7 @@ function initialize() {
     if (!result.error) {
       var container = $("#feed");
       _.each(result.feed.entries, function(entry){
-        var div = $("<div></div>").addClass("entry");
+        var div = $("<div></div>").addClass("entry row");
         // store entry in jQuery data attribute
         div.data("talk-info", entry);
         // using lodash template
@@ -28,8 +30,17 @@ $(document).on("click", ".entry", function(){
   var talkInfo = $(this).data("talk-info");
   // using lodash template
   compiledModal = modalTemplate(talkInfo);
-  $(".remodal").append(compiledModal);
+  $(".modalContent").html('');
+  $(".modalContent").append(compiledModal);
+
+  var options = {};
+  $('[data-remodal-id=modal]').remodal(options);
+  var inst = $('[data-remodal-id=modal]').remodal();
+  inst.open();
 })
+
 $(window).on("unload", function(){
-  console.log("fart");
+  inst.close();
+  inst.destroy();
+  this.location = this.location.origin;
 })
--- public/templates.js
+++ public/templates.js
@@ -1,9 +1,11 @@
-var entryTemplate = _.template("<article><h3><%=title %></h3><p><%=contentSnippet %></p>" +
-  "<h5><a href=<%=link %>>Watch this Ted Talk</a></h5>" +
-  "<h5><a href='#modal'>More Info</a></h5>" +
+var entryTemplate = _.template("<article>" +
+  "<h5><%=title %></h5><p><a href='#modal'><%=contentSnippet %></a></p>" +
+  "<span class='linkSpan'><a href=<%=link %>>Watch this Ted Talk</a></span>" +
+  "<span class='moreInfoSpan'><a href='#modal'>More Info</a></span>" +
   "</article>"
 );
 
-var modalTemplate = _.template("<h4><%= title %></h4>" +
-  "<h5><a href=<%=link %>>Watch this Ted Talk</a></h5>" 
+var modalTemplate = _.template("<h3><%= title %></h3>" +
+  "<p><%= content %></p>" +
+  "<p><a href=<%=link %>>Watch this Ted Talk</a></p>"
 );
