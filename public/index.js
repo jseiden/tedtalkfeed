8643fb57f9fe109d358d053cfeadfa08282798eb
jseiden <jseiden@gmail.com>
2015-10-09 19:29:51 -0700

    style edits and js fixes

--- public/index.js
+++ public/index.js
@@ -1,46 +1,57 @@
+(function(){
 
+  google.load("feeds", "1");
 
-google.load("feeds", "1");
+  function initialize() {
+    var feed = new google.feeds.Feed("http://feeds.feedburner.com/tedtalks_video");
+    feed.includeHistoricalEntries();
+    feed.setNumEntries(20);
+    feed.load(function(result) {
+      if (!result.error) {
+        var container = $("#feed");
+        _.each(result.feed.entries, function(entry){
+          var div = $("<div></div>").addClass("entry row");
+          // store entry in jQuery data attribute
+          div.data("talk-info", entry);
+          // using lodash template
+          compiledEntry = entryTemplate(entry);
+          div.append(compiledEntry);
+          console.log(entry);
+          // div.append(entry.content);
+          container.append(div);
+        })
+      }
+    });
+  }
+  google.setOnLoadCallback(initialize);
 
-function initialize() {
-  var feed = new google.feeds.Feed("http://feeds.feedburner.com/tedtalks_video");
-  feed.includeHistoricalEntries();
-  feed.setNumEntries(20);
-  feed.load(function(result) {
-    if (!result.error) {
-      var container = $("#feed");
-      _.each(result.feed.entries, function(entry){
-        var div = $("<div></div>").addClass("entry row");
-        // store entry in jQuery data attribute
-        div.data("talk-info", entry);
-        // using lodash template
-        compiledEntry = entryTemplate(entry);
-        div.append(compiledEntry);
-        // div.append(entry.content);
-        container.append(div);
-      })
+  $(".linkSpan").on("click", function(){
+    event.stopPropagation();
+  })
+
+  // click event on 'more info' link bubbles up to entry div
+  $(document).on("click", ".entry", function(){
+    // retrieve stored entry from jQuery data attribute
+    if($(event.target).is(".info")){
+      console.log("winner");
+      var talkInfo = $(this).data("talk-info");
+      // using lodash template
+      compiledModal = modalTemplate(talkInfo);
+      $(".modalContent").html('');
+      $(".modalContent").append(compiledModal);
+
+      var options = {};
+      $('[data-remodal-id=modal]').remodal(options);
+      var inst = $('[data-remodal-id=modal]').remodal();
+
+      inst.open();
     }
-  });
-}
-google.setOnLoadCallback(initialize);
+  })
 
-// click event on 'more info' link bubbles up to entry div
-$(document).on("click", ".entry", function(){
-  // retrieve stored entry from jQuery data attribute
-  var talkInfo = $(this).data("talk-info");
-  // using lodash template
-  compiledModal = modalTemplate(talkInfo);
-  $(".modalContent").html('');
-  $(".modalContent").append(compiledModal);
+  $(window).on("unload", function(){
+    inst.close();
+    inst.destroy();
+    this.location = this.location.origin;
+  })
 
-  var options = {};
-  $('[data-remodal-id=modal]').remodal(options);
-  var inst = $('[data-remodal-id=modal]').remodal();
-  inst.open();
-})
-
-$(window).on("unload", function(){
-  inst.close();
-  inst.destroy();
-  this.location = this.location.origin;
-})
+})();
--- public/templates.js
+++ public/templates.js
@@ -1,11 +1,13 @@
 var entryTemplate = _.template("<article>" +
-  "<h5><%=title %></h5><p><a href='#modal'><%=contentSnippet %></a></p>" +
+  "<h4><%=title %></h4><p><%=contentSnippet %></p>" +
   "<span class='linkSpan'><a href=<%=link %>>Watch this Ted Talk</a></span>" +
-  "<span class='moreInfoSpan'><a href='#modal'>More Info</a></span>" +
+  "<span class='moreInfoSpan'><a href='#modal' class='info'>More Info</a></span>" +
   "</article>"
 );
 
-var modalTemplate = _.template("<h3><%= title %></h3>" +
-  "<p><%= content %></p>" +
-  "<p><a href=<%=link %>>Watch this Ted Talk</a></p>"
+var modalTemplate = _.template("<h4><%= title %></h4>" +
+  "<blockquote><%= content %></blockquote>" +
+  "<p><a href=<%=link %>>Watch this Ted Talk</a></p>" +
+  "<span class='dateSpan'>Originally posted on <% var date = new Date(publishedDate); print(date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear()) %></span>" +
+  "<span class='categorySpan'>Category: <%=categories %></span>"
 );
